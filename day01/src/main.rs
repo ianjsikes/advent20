b363e467fc0fc6ffd0a7089259408543a11ced30
Ian J Sikes <ianjsikes@gmail.com>
2020-12-02 10:54:40 -0800

    Move day2 solution to day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,7 +2,11 @@ use std::collections::HashSet;
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    part_one();
+    part_two();
+}
+
+pub fn part_one() {
     let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
 
     let numbers: Vec<i32> = contents.lines().map(|l| l.parse().unwrap()).collect();
@@ -12,9 +16,31 @@ fn main() {
     for number in numbers {
         let diff = target - number;
         if diff_set.contains(&diff) {
-            println!("Solution: {}", diff * number);
+            println!("Part One Solution: {}", diff * number);
             return;
         }
         diff_set.insert(number);
+    }
+}
+
+pub fn part_two() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let numbers: Vec<i32> = contents.lines().map(|l| l.parse().unwrap()).collect();
+    let target: i32 = 2020;
+
+    for (idx, num1) in numbers.iter().enumerate() {
+        let inner_target = target - num1;
+        let mut diff_set: HashSet<i32> = HashSet::new();
+
+        for x in (idx + 1)..(numbers.len() - 1) {
+            let num2 = numbers[x];
+            let diff = inner_target - num2;
+            if diff_set.contains(&diff) {
+                println!("Part Two Solution {}", diff * num2 * num1);
+                return;
+            }
+            diff_set.insert(num2);
+        }
     }
 }
