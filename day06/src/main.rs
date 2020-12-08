62249e1a532b78a6cbb5632faf8e13f57af70b02
Ian J Sikes <ianjsikes@gmail.com>
2020-12-07 16:07:49 -0800

    Day 6 solved!

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, io};
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() -> io::Result<()> {
+    let mut first_sum = 0;
+    let mut second_sum = 0;
+    for group in INPUT.split("\n\n") {
+        let mut answers: HashMap<char, usize> = HashMap::new();
+        let lines = group.lines();
+        let num_people = lines.clone().count();
+        for line in lines {
+            for c in line.chars() {
+                *answers.entry(c).or_insert(0) += 1;
+            }
+        }
+        let count = answers.iter().fold(
+            0,
+            |sum, (_, &num)| if num == num_people { sum + 1 } else { sum },
+        );
+        first_sum += answers.len();
+        second_sum += count;
+    }
+    println!("Part One: {}", first_sum);
+    println!("Part Two: {}", second_sum);
+    Ok(())
 }
